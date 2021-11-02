c8e1ac5bdd859aa1923ccdeca7cc75b2c76aeebe
Zak003 <59819711+Zak003@users.noreply.github.com>
2021-11-02 12:57:25 +0100

    add user name, edit profile link

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,25 @@
 module PostsHelper
+    #funkcija
+    def display_index_post_links(post)
+        capture do #brez capture do nam podatke dvakrat izpise
+            concat content_tag(:td, link_to('Show', post)) #content_tag()je funkcija, ki sprejme najprej kateri tag naredi(:tr), v drugem pa kaj bo not v tem tagu
+            if current_user == post.user
+                concat content_tag(:td, link_to('Edit', edit_post_path(post)))
+                concat content_tag(:td, link_to('Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }))
+            end
+            #html.html_safe #tega ne potrebujemo če uporabimo concat namesto html+=
+        end
+    end
+
+    #funkcija
+    def display_show_post_links(post)
+        capture do 
+            if current_user == post.user
+                concat link_to 'Edit', edit_post_path(post) #ruby definiramo s #{} ostalo je html
+                concat ' | '
+            end
+            concat link_to 'Back', posts_path
+            #html.html_safe
+        end
+    end
 end
