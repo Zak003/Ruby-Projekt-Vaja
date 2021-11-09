5a945ff772b911f1eb348f587fe5b891f5f5187e
Zak003 <59819711+Zak003@users.noreply.github.com>
2021-11-09 20:05:00 +0100

    fix post authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   #before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create ] #ali ri ali spodnja vrstica
-  before_action :authenticate_user!, except: [:index, :show ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authorize_user!, except: [:index, :show]
 
   # GET /posts or /posts.json
   def index
@@ -20,6 +21,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
+    #raise @post.to_yaml
   end
 
   # POST /posts
@@ -70,5 +72,9 @@ class PostsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :body)
+    end
+
+    def authorize_user!
+      redirect_back fallback_location: root_path, alert: 'Nimate dostopa do te strani.' unless current_user == @post.user #preusmeri te na stran s katere si prišel
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
 
     belongs_to :user #post pripada userju, tukaj bo user_id
+
+    validates :title, presence: true
+    validates :body, presence: true
 end
