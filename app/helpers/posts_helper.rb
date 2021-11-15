e9581a0e8bfec154ed1a6ecb3f674e737b797fe3
Zak003 <59819711+Zak003@users.noreply.github.com>
2021-11-15 22:35:45 +0100

    fix helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,10 +3,10 @@ module PostsHelper
     def display_index_post_links(post)
         capture do #brez capture do nam podatke dvakrat izpise
             concat link_to('Preberi več', post, class: 'stretched-link') #content_tag()je funkcija, ki sprejme najprej kateri tag naredi(:tr), v drugem pa kaj bo not v tem tagu
-            if current_user == post.user
-                concat link_to('Uredi', edit_post_path(post), class: 'stretched-link')
-                concat link_to('Izbriši', post, method: :delete, data: { confirm: 'Are you sure?' }, class: 'stretched-link')
-            end
+            #if current_user == post.user
+                #concat link_to('Uredi', edit_post_path(post), class: 'stretched-link')
+                #concat link_to('Izbriši', post, method: :delete, data: { confirm: 'Are you sure?' }, class: 'stretched-link')
+            #end
             #html.html_safe #tega ne potrebujemo če uporabimo concat namesto html+=
         end
     end
@@ -15,6 +15,8 @@ module PostsHelper
     def display_show_post_links(post)
         capture do 
             if current_user == post.user
+                concat link_to 'Izbriši', post, method: :delete, data: { confirm: 'Are you sure?' }
+                concat ' | '
                 concat link_to 'Edit', edit_post_path(post) #ruby definiramo s #{} ostalo je html
                 concat ' | '
             end
